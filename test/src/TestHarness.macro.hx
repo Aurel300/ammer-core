93105392b49789daf7ea86ceb558c8ea33445707
Aurel Bílý <aurel.bily@gmail.com>
2022-09-25 21:34:19 +0200

    enable float32 tests

--- test/src/TestHarness.macro.hx
+++ test/src/TestHarness.macro.hx
@@ -93,7 +93,6 @@ class TestHarness {
 
     var testExprs:Array<Expr> = [];
     for (ctor in ([
-      test.TestHaxe.new,
       #if !AMMER_TEST_CS
         test.TestArrays.new,
       #end
@@ -101,6 +100,7 @@ class TestHarness {
       test.TestBytes.new,
       test.TestCallbacks.new,
       test.TestFloats.new,
+      test.TestHaxe.new,
       test.TestIntegers.new,
       test.TestStrings.new,
       test.TestStructs.new,
--- test/src/test/TestFloats.hx
+++ test/src/test/TestFloats.hx
@@ -6,16 +6,14 @@ class TestFloats extends TestBase {
   public function new() {
     super("TestFloats");
     for (kind in [
-      #if !(AMMER_TEST_EVAL || AMMER_TEST_LUA || AMMER_TEST_NEKO || AMMER_TEST_NODEJS || AMMER_TEST_PYTHON)
       {m: marshal.float32() },
-      #end
       {m: marshal.float64() },
       // {m: marshal.float128()},
     ]) {
       var f = lib.addFunction(kind.m, [kind.m], '_return = _arg0 * 2.0;');
       // TODO: epsilon?
-      assertEq(macro $f(3.21),  macro 6.42 , kind.m.haxeType);
-      assertEq(macro $f(-3.21), macro -6.42, kind.m.haxeType);
+      assertEq(macro $f(3.5),  macro 7.0,  kind.m.haxeType);
+      assertEq(macro $f(-3.5), macro -7.0, kind.m.haxeType);
     }
   }
 }
